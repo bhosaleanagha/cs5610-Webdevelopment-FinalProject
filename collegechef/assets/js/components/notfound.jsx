e7953db9ff053fa840bed80244488870b7fcc2f0
Anagha Bhosale <anaghabhosale@Anaghas-MacBook-Air.local>
2019-11-24 23:44:03 -0500

    anagha

---
+++ collegechef/assets/js/components/notfound.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Alert, FormGroup, Form } from 'react-bootstrap';
+import { connect } from 'react-redux';
+
+const NotFound = ({ location }) => (
+    <div>
+      <h3>No match for <code>{location.pathname}</code></h3>
+     <h1>404 Page not Found</h1>
+    </div>
+  )
+
+  function state2props(state) {
+    return state.forms.notfound;
+  }
+  export default connect(state2props)(NotFound);
